d068c6747c6d4205d957161790f980fab9a2e2ca
yibarr <ibgayael@gmail.com>
2019-07-13 23:50:31 -0500

    added components folder Inv..., Home, Transf...

---
+++ src/components/Investment/Investment.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Investment extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Investment
---
+++ src/components/Transference/Transference.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Transference extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Transference
