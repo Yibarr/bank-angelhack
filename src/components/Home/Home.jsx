30b719cc21bc8866bcc7b64d180f9ce8cc1f40e1
victor117 <rubensolaya@gmail.com>
2019-07-14 07:22:44 -0500

    Consuming back api

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export class Home extends Component {
+    constructor(props){
+        super(props);
+        state={
+            enterprise=[],
+            clients=[]
+        }
+    }
+
+    componentDidMount(){
+        axios.get(`https://angel-bank-backend.herokuapp.com/get/pymes`)
+        .then(res=>{
+            const enterprise = res.data;
+            this.setState({enterprise});
+        })
+        axios.get(`clientAPI`)
+        .then(res=>{
+            const clients = res.data;
+            this.setState({clients});
+        })
+
+    }
+
+        
+    
+
     render() {
         return (
             <div>
-                
+                <h2>hey</h2>
             </div>
         )
     }
